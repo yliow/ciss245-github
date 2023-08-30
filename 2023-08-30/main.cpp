9e508366a1851510b263bb311725a645754315c0
Yihsiang Liow <yliow@ccis.edu>
2023-08-30 15:54:15 -0400

    no msg

--- 2023-08-30/main.cpp
+++ 2023-08-30/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 int die_roll();
+char coin_toss();
 
 int main()
 {
@@ -14,11 +15,30 @@ int main()
         std::cout << die_roll() << ' ';
     }
     std::cout << '\n';
-    
+
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << coin_toss() << ' ';
+    }
+    std::cout << '\n';
+
     return 0;
 }
 
 int die_roll()
 {
     return rand() % 6 + 1;
+}
+
+char coin_toss()
+{
+    int r = rand() % 2;
+    if (r == 0)
+    {
+        return 'T';
+    }
+    else
+    {
+        return 'H';
+    }
 }
