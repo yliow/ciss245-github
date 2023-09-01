74c6d64ab260196bd3940161aec1c8f1cc9ed5d1
Yihsiang Liow <yliow@ccis.edu>
2023-09-01 16:11:59 -0400

    no msg

--- 2023-09-01/main.cpp
+++ 2023-09-01/main.cpp
@@ -4,12 +4,36 @@
 
 const int SIZE = 1000;
 
-void rand_array(int x[], int n)
+void rand_array(int x[], int start, int end)
 {
-    for (int i = 0; i < n; ++i)
+    for (int i = start; i < end; ++i)
     {
         x[i] = rand() % 20;
     }
+}
+
+void change(int & i)
+{
+    i = 0;
+}
+
+void println(int x[], int start,  int end)
+{
+    for (int i = start; i < end; ++i)
+    {
+        std::cout << x[i] << ' ';
+    }
+    std::cout << '\n';    
+}
+
+int linearsearch(int x[], int start, int end,
+                 int target)
+{
+    for (int i = start; i < end; ++i)
+    {
+        std::cout << x[i] << ' ';
+    }
+    std::cout << '\n';        
 }
 
 int main()
@@ -20,14 +44,13 @@ int main()
     x[0] = 42;
     x[1] = 1;
     x[2] = -4;
+    println(x, 0, 10);
+    rand_array(x, 0, 10);
+    println(x, 0, 10);
 
-    for (int i = 0; i < 10; ++i)
-    {
-        std::cout << x[i] << ' ';
-    }
-    std::cout << '\n';
-
-    rand_array(x, 10);
+    // int i = 42;
+    // change(i);
+    // std::cout << i << '\n';
     
     // std::vector< int > v(10);
     // v[0] = 42;
