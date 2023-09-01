2b4cfc37395821c15607bd920d7479c58edb5bd6
Yihsiang Liow <yliow@ccis.edu>
2023-09-01 15:54:51 -0400

    no msg

--- 2023-09-01/main.cpp
+++ 2023-09-01/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 
 const int SIZE = 1000;
 
+void rand_array(int x[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        x[i] = rand() % 20;
+    }
+}
+
 int main()
 {
+    srand(0);
+    
     int x[SIZE];
-
     x[0] = 42;
     x[1] = 1;
     x[2] = -4;
@@ -15,6 +26,18 @@ int main()
         std::cout << x[i] << ' ';
     }
     std::cout << '\n';
+
+    rand_array(x, 10);
     
+    // std::vector< int > v(10);
+    // v[0] = 42;
+    // v[1] = 1;
+    // v[2] = -4;
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     std::cout << v[i] << ' ';
+    // }
+    // std::cout << '\n';
+        
     return 0;
 }
