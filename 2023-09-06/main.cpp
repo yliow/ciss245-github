915bc972035c461d6de9c5d94daa27689f151379
Yihsiang Liow <yliow@ccis.edu>
2023-09-06 16:23:31 -0400

    no msg

--- 2023-09-06/main.cpp
+++ 2023-09-06/main.cpp
@@ -38,7 +38,7 @@ void println(int ca[], int n)
 {
     for (int i = 0; i < 2 * n + 1; ++i)
     {
-        std::cout << (ca[i] == 1 ? 'X' : ' ')
+        std::cout << (ca[i] == 1 ? 'X' : ' ');
     }
     std::cout << '\n';
 }
