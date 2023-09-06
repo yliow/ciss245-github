bba3869fd60c0cc3dd0d56fad20c25516230f6aa
Yihsiang Liow <yliow@ccis.edu>
2023-09-06 16:20:07 -0400

    no msg

--- 2023-09-06/main.cpp
+++ 2023-09-06/main.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 
-int binarysearch(int x[], int start, int end,
-                 int target)
+// int binarysearch(int x[], int start, int end,
+//                  int target)
+// {
+//     int left = start;
+//     int right = end - 1;
+//     while (left <= right)
+//     {
+//         int mid = (left + right) / 2;
+//         std::cout << left << ' ' << mid << ' ' << right << '\n';
+//         if (x[mid] == target)
+//         {
+//             return mid;
+//         }
+//         else if (x[mid] < target)
+//         {
+//             left = mid + 1;
+//         }
+//         else
+//         {
+//             right = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
+
+void init(int ca[], int n)
 {
-    int left = start;
-    int right = end - 1;
-    while (left <= right)
+    for (int i = 0; i < 2 * n + 1; ++i)
     {
-        int mid = (left + right) / 2;
-        std::cout << left << ' ' << mid << ' ' << right << '\n';
-        if (x[mid] == target)
-        {
-            return mid;
-        }
-        else if (x[mid] < target)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
+        ca[i] = 0;
     }
-    return -1;
+    ca[n] = 1;
 }
 
 int main()
 {
-    int x[] = {1, 4, 5, 7, 9, 15, 19, 26};
-    int index = binarysearch(x, 0, 8, 5);
+    // int x[] = {1, 4, 5, 7, 9, 15, 19, 26};
+    // int index = binarysearch(x, 0, 8, 5);
+
+    int ca[2 * 500 + 1];
+    int t[2 * 500 + 1];
+    int n;
+    std::cin >> n;
+
+    init(ca, n);
+    
     return 0;
 }
