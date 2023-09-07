91948fcb21a763ca16b19814d47bdd19c5afeac8
Yihsiang Liow <yliow@ccis.edu>
2023-09-07 15:38:14 -0400

    no msg

---
+++ 2023-09-07/chess.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void init(char chess[8][8])
+{
+    for (int r = 2; r < 6; ++r)
+    {
+        for (int c = 0; c < 8; ++c)
+        {
+        }
+    }
+}
+
+int main()
+{
+    char chess[8][8];
+    init(chess);
+    println(chess);
+
+    return 0;
+}
