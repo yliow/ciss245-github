2c5b1cc32fa72597619122ae156eb3a1016695b2
Yihsiang Liow <yliow@ccis.edu>
2023-09-07 15:47:23 -0400

    no msg

--- 2023-09-07/chess.cpp
+++ 2023-09-07/chess.cpp
@@ -2,6 +2,17 @@
 
 void init(char chess[8][8])
 {
+    char pieces[] = "rnbqkbnr";
+    for (int c = 0; c < 8; ++c)
+    {
+        chess[0][c] = pieces[c];
+        chess[7][c] = 'A' + pieces[c] - 'a';
+    }
+    for (int c = 0; c < 8; ++c)
+    {
+        chess[1][c] = 'p';
+        chess[6][c] = 'P';
+    }
     for (int r = 2; r < 6; ++r)
     {
         for (int c = 0; c < 8; ++c)
