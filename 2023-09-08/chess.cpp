3a7c3114c00c7ccfebff54db815e999197bdcb2f
Yihsiang Liow <yliow@ccis.edu>
2023-09-08 15:45:33 -0400

    no msg

--- 2023-09-08/chess.cpp
+++ 2023-09-08/chess.cpp
@@ -40,7 +40,7 @@ void println(char chess[8][8])
     }
 }
 
-void get_move(char chess[8][8], int & r0, int &c0, int &r1, int &c1)
+void get_move(char chess[8][8], char turn, int & r0, int &c0, int &r1, int &c1)
 {
     println(chess);
     std::cout << turn << "'s turn\n"
@@ -60,8 +60,9 @@ int main()
     char turn = 'W';
     while (1)
     {
-        // make move
-        // check move correctness
+        // get the move from user
+        int r0, c0, r1, c1;
+        get_move(chess, turn, r0, c0, r1, c1);
 
         // switch turn
         turn = (turn == 'W' ? 'B' : 'W');
