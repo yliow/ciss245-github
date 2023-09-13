671b75c193b1d9345583479077b3ec54f8cd28ad
Yihsiang Liow <yliow@ccis.edu>
2023-09-13 15:55:28 -0400

    no msg

---
+++ 2023-09-13/math-demo/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int max(int x, int y);
+
+int main()
+{
+    std::cout << max(3, 5) << '\n';
+    return 0;
+}
+
---
+++ 2023-09-13/math-demo/mymath.cpp
@@ -0,0 +1,6 @@
+// file: mymath.cpp
+
+int max(int x, int y)
+{
+    return (x >= y ? x : y);
+}
