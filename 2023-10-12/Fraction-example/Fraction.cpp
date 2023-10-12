2dba0021cdd1c9d33bb9de651af69f4efa374554
Yihsiang Liow <yliow@ccis.edu>
2023-10-12 15:58:42 -0400

    no msg

---
+++ 2023-10-12/Fraction-example/Fraction.cpp
@@ -0,0 +1,28 @@
+// Fraction.cpp
+// for maintainer of library
+
+#include <iostream>
+#include "Fraction.h"
+
+Fraction get_Fraction(int n, int d)
+{
+    Fraction ret = {n, d};
+    return ret;
+}
+
+std::ostream & operator<<(std::ostream & cout, const
+                          Fraction & f)
+{
+    cout << f.n;
+    if (f.d != 1)
+    {
+        cout << '/' << f.d;
+    }
+    return cout;
+}
+
+bool operator==(const Fraction & f0, const Fraction & f1)
+{
+    return (f0.n * f1.d == f0.d * f1.n);
+}
+
---
+++ 2023-10-12/Fraction-example/main.cpp
@@ -0,0 +1,16 @@
+// main.cpp
+
+#include <iostream>
+#include "Fraction.h"
+
+int main()
+{
+    Fraction f0 = get_Fraction(5, 24);
+    std::cout << f0 << '\n';
+    Fraction f1 = get_Fraction(5, 1);
+    std::cout << f1 << '\n';
+
+    std::cout << (get_Fraction(1, 2) == get_Fraction(2, 4))
+              << '\n';
+    return 0;
+}
