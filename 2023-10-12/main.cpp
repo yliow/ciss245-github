ee582555eeb03463329547ec83cc7f4ade6d2ead
Yihsiang Liow <yliow@ccis.edu>
2023-10-12 15:12:36 -0400

    no msg

---
+++ 2023-10-12/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+// file: Date.h
+//#ifndef .....
+struct Date
+{
+    int yyyy, mm, dd;
+};
+
+std::ostream & operator<<(std::ostream & cout, const Date &);
+void init(Date & date, int hhhh, int mm, int dd);
+bool is_equal(const Date &, const Date &);
+bool operator==(const Date & d0, const Date & d1);
+Date operator+(const Date & d0, const Date & d1);
+
+// file: Date.cpp
+//#include "Date.h"
+
+std::ostream & operator<<(std::ostream & cout, const Date & date)
+{
+    cout << date.yyyy << '/' << date.mm << '/' << date.dd;
+    return cout;
+}
+
+void init(Date & date, int yyyy, int mm, int dd)
+{
+    date.yyyy = yyyy;
+    date.mm = mm;
+    date.dd = dd;
+}
+
+bool is_equal(const Date & d0, const Date & d1)
+{
+    return (d0.yyyy == d1.yyyy && d0.mm == d1.mm && d0.dd == d1.dd);
+}
+
+bool operator==(const Date & d0, const Date & d1)
+{
+    return (d0.yyyy == d1.yyyy && d0.mm == d1.mm && d0.dd == d1.dd);
+}
+
+Date operator+(const Date & d0, const Date & d1)
+{
+    Date ret;
+    init(ret, d0.yyyy + d1.yyyy, d0.mm + d1.mm, d0.dd + d1.dd);
+    return ret;
+}
+
+
+int main()
+{
+    Date today;
+    // today.yyyy = 2023;
+    // today.mm = 10;
+    // today.dd = 11;
+    init(today, 2023, 10, 11);
+
+    //std::cout << today.yyyy << '/' << today.mm << '/' << today.dd << '\n';
+    std::cout << today << '\n';
+
+    Date tomorrow;
+    init(tomorrow, 0, 0, 0);
+    tomorrow = today;
+    std::cout << tomorrow << '\n';
+
+    std::cout << is_equal(tomorrow, today) << '\n';
+    std::cout << (tomorrow == today) << '\n'; 
+    Date day;
+    init(day, 0, 0, 1);
+    tomorrow = today + day; // today+day = operator+(today, day)
+    std::cout << tomorrow << '\n';
+    return 0;
+}
