257fd65aaabf7faa31634493f978f12a996b6ce7
Yihsiang Liow <yliow@ccis.edu>
2023-10-16 17:33:13 -0400

    no msg

---
+++ 2023-10-16/main.cpp
@@ -0,0 +1,89 @@
+// templates in a sep library MUST be in a header file
+
+#include <iostream>
+
+template < typename T > // template < class T >
+T max(T x, T y)
+{
+    return (x >= y ? x : y);
+}
+
+// int max(int x, int y)
+// {
+//     return (x >= y ? x : y);
+// }
+
+// double max(double x, double y)
+// {
+//     return (x >= y ? x : y);
+// }
+
+template < typename T > 
+void swap(T & x, T & y)
+{
+    T t = x;
+    x = y;
+    y = t;
+}
+
+// void swap(int & x, int & y)
+// {
+//     int t = x;
+//     x = y;
+//     y = t;
+// }
+
+// void swap(double & x, double & y)
+// {
+//     double t = x;
+//     x = y;
+//     y = t;
+// }
+
+template < typename S, typename T >
+S avg(T x, T y)
+{
+    return (x + y) / 2;
+}
+
+template < typename T >
+struct Array
+{
+    T x[1024];
+    int capacity;
+    int size;
+};
+
+template < typename T >
+void init(Array< T > & a)
+{
+    a.capacity = 1024;
+    a.size = 0;
+}
+
+template < typename T >
+void println(Array< T > & a)
+{
+    std::cout << '{';
+    for (int i = 0; i < a.size; ++i)
+    {
+        std::cout < a.x[i] << ' '; 
+    }
+    std::cout << '}' << '\n';
+}
+
+int main()
+{
+    std::cout << max< int >(3, 5) << '\n';
+    std::cout << max(3, 5) << '\n';
+
+    int x = 0, y = 1;
+    swap< int >(x, y);
+    std::cout << x << ' ' << y << '\n';
+
+    float a = avg< float, double >(3.4, 5.4);
+
+    Array< int > a;
+    init(a);
+    return 0;
+}
