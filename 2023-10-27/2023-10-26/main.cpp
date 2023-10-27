6d4eacd823499cd92bcf760b1c67eac5de6a779f
Yihsiang Liow <yliow@ccis.edu>
2023-10-27 18:03:09 -0400

    no msg

---
+++ 2023-10-27/2023-10-26/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+// file: Array.h
+template < typename T >
+class Array
+{
+public:
+    Array()
+        : size_(0), capacity_(1024)
+    {
+        std::cout << "Array::Array() ... \n";
+    }
+    Array(int size)
+        : size_(size), capacity_(1024)
+    {
+        std::cout << "Array::Array(int) ... \n";
+    }
+    int size() const
+    {
+        return size_;
+    }
+    T operator[](int i) const
+    {
+        return x_[i];
+    }
+private:
+    T x_[1024];
+    int size_;
+    int capacity_;
+};
+
+template < typename T >
+std::ostream & operator<<(std::ostream & cout, const Array< T > & a)
+{
+    cout << '{';
+    for (int i = 0; i < a.size(); ++i)
+    {
+        cout << a[i] << ' '; // a[i] same as a.operator[](i)
+    }
+    cout << '}';
+    return cout;
+}
+
+int main()
+{
+    Array< int > a;
+    Array< int > b(10);
+    std::cout << b << '\n';
+    //Array< double > b;
+    
+    return 0;
+}
