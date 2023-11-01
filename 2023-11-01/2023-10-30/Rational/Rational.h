8c9a0667ac178c0b6111aeb9ae58121fb89901e5
Yihsiang Liow <yliow@ccis.edu>
2023-11-01 16:22:10 -0400

    no msg

---
+++ 2023-11-01/2023-10-30/Rational/Rational.h
@@ -0,0 +1,30 @@
+// file: Rational.h
+
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+#include <iostream>
+
+class Rational
+{
+  public:
+    Rational();
+    Rational(int n, int d);
+    Rational(const Rational &);
+    int n() const /* means *this cannot be changed */;
+    int & n();
+    int d() const;
+    int & d();
+    const Rational & operator+=(const Rational &); /* const ... WRONG */
+    Rational operator+(const Rational & r) const;
+    const Rational & operator-=(const Rational &);
+    Rational operator-(const Rational & r) const;
+    const Rational & operator=(const Rational & r);
+  private:
+    int n_, d_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const Rational &);
+std::istream & operator>>(std::istream & cin, Rational &);
+
+#endif
