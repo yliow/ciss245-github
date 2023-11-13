3ce0a9c9232100d548c840fce7c87919908fd682
Yihsiang Liow <yliow@ccis.edu>
2023-11-13 15:41:39 -0500

    no msg

---
+++ 2023-11-13/IntPointer/IntPointer.h
@@ -0,0 +1,21 @@
+#ifndef INTPOINTER_H
+#define INTPOINTER_H
+
+#include <iostream>
+
+class IntPointer
+{
+  public:
+    IntPointer(int);
+    IntPointer(const IntPointer &);
+    ~IntPointer();
+    int * get_p() const;
+    int dereference() const;
+    const IntPointer & operator=(const IntPointer &);
+  private:
+    int * p_;
+};
+
+std::ostream & operator<<(std::ostream &, const IntPointer &);
+
+#endif
