145c112c0b6c79f0495152a16add610b9980fa99
Yihsiang Liow <yliow@ccis.edu>
2023-11-15 15:44:25 -0500

    no msg

---
+++ 2023-11-15/DynIntArray/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "DynIntArray.h"
+
+int main()
+{
+    DynIntArray a(5); // want array of size 5
+    std::cout << "a: " << a << '\n'; // want to see {0, 0, 0, 0, 0}; a points to size 5
+    a[1] = 42;
+    std::cout << "a: " << a << '\n';
+    
+    DynIntArray b(a);
+    std::cout << "b: " << b << '\n';
+
+    b = a;
+    b.push_back(100);
+    std::cout << "b: " << b << ' ' << b.capacity() << '\n';
+    return 0;
+}
