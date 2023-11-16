3c0db90f3ee8a606e3256aeca1eb6ece8c22112e
Yihsiang Liow <yliow@ccis.edu>
2023-11-16 15:53:22 -0500

    no msg

--- 2023-11-16/recursion/main.cpp
+++ 2023-11-16/recursion/main.cpp
@@ -8,26 +8,78 @@
 //     f(n);
 // }
 
-void f(int n)
+// void f(int n)
+// {
+//     if (n <= 0)
+//     {
+//         // base case
+//         std::cout << "time to stop\n";
+//     }
+//     else
+//     {
+//         // recursive case
+//         std::cout << n << '\n';
+//         char c;
+//         std::cin >> c;
+//         f(n - 1);
+//     }
+// }
+
+// void f(int n)
+// {
+//     if (n <= 0)
+//     {
+//         // base case
+//         std::cout << "time to stop\n";
+//     }
+//     else
+//     {
+//         // recursive case
+//         std::cout << n << '\n';
+//         //char c;
+//         //std::cin >> c;
+//         f(n + 1);
+//     }
+// }
+
+// void f(int n)
+// {
+//     if (n <= 0)
+//     {
+//         // base case
+//         std::cout << "time to stop\n";
+//     }
+//     else
+//     {
+//         // recursive case
+//         std::cout << n << '\n';
+//         char c;
+//         std::cin >> c;
+//         f(n - 2);
+//     }
+// }
+
+int sum(int n)
 {
-    if (n <= 0)
+    if (n == 0)
     {
-        // base case
-        std::cout << "time to stop\n";
+        std::cout << "sum n = 0 ... returning 0\n";
+        return 0;
     }
     else
     {
         // recursive case
-        std::cout << n << '\n';
-        char c;
-        std::cin >> c;
-        f(n - 1);
+        int t = sum(n - 1);
+        //std::cout << "t:" << t << '\n';
+        std::cout << "sum n = " << n << " ... returning " << t  + n << "\n";
+        return t + n;
     }
 }
 
-
 int main()
 {
-    f(5);
+    //f(5);
+    int ret = sum(5);
+    std::cout << "final:" << ret << '\n';
     return 0;
 }
