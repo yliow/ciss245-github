163792015fa39b1ced457c2a4d2d2f824bffe041
Yihsiang Liow <yliow@ccis.edu>
2023-12-04 15:35:23 -0500

    no msg

--- 2023-12-01/main.cpp
+++ 2023-12-01/main.cpp
@@ -13,48 +13,48 @@
 
  */
 
-int max(int x[], int n)
-{
-    if (n == 0)
-    {
-        return -1;
-    }
-    else
-    {
-        int m = max(x, n - 1);
-        if (m > x[n]) return m;
-        else return x[n];
-    }
-}
+// int max(int x[], int n)
+// {
+//     if (n == 0)
+//     {
+//         return -1;
+//     }
+//     else
+//     {
+//         int m = max(x, n - 1);
+//         if (m > x[n]) return m;
+//         else return x[n];
+//     }
+// }
 
-int fib(int n)
-{
-    if (n == 0 || n == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return fib(n - 1) + fib(n - 2);
-    }
-}
+// int fib(int n)
+// {
+//     if (n == 0 || n == 1)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
 
-// memoization technique
-int table[1000];
-int fib2(int n)
-{
-    if (n == 0 || n == 1) return 1;
-    else
-    {
-        // if fib(n) is in table, use table.
-        // if not, compute it and store in table.
-        if (table[n] == 0)
-        {
-            table[n] = fib2(n - 1) + fib2(n - 2);
-        }
-        return table[n];
-    }
-}
+// // memoization technique
+// int table[1000];
+// int fib2(int n)
+// {
+//     if (n == 0 || n == 1) return 1;
+//     else
+//     {
+//         // if fib(n) is in table, use table.
+//         // if not, compute it and store in table.
+//         if (table[n] == 0)
+//         {
+//             table[n] = fib2(n - 1) + fib2(n - 2);
+//         }
+//         return table[n];
+//     }
+// }
 
 void println(int x[], int n)
 {
@@ -82,30 +82,42 @@ void bubblesort_pass(int x[], int j, int i)
     }
 }
 
+void bubblesort(int x[], int i)
+{
+    if (?) // base case
+    {}
+    else // recursive case
+    {
+        bubblesort_pass(x, i);
+        bubblesort(x, i - 1);
+    }
+}
+
 int main()
 {
     // int x[] = {3, 5, 2, 6, 7};
     // int m = max(x, 5);
     // std::cout << "m:" << m << '\n';
 
-    recursive case for fibo
-    for (int n = 0; n < 50; ++n)
-    {
-        std::cout << n << " ... " << fib(n) << '\n';
-    }
+    // recursive case for fibo
+    // for (int n = 0; n < 50; ++n)
+    // {
+    //     std::cout << n << " ... " << fib(n) << '\n';
+    // }
 
-    recursive with table lookup (memorization) for fibo
-    for (int i = 0; i < 50; ++i)
-    {
-        table[i] = 0; // to indicate not yet computed
-    }
-    for (int n = 0; n < 50; ++n)
-    {
-        std::cout << n << " ... " << fib2(n) << '\n';
-    }
-
+    //recursive with table lookup (memorization) for fibo
+    // for (int i = 0; i < 50; ++i)
+    // {
+    //     table[i] = 0; // to indicate not yet computed
+    // }
+    // for (int n = 0; n < 50; ++n)
+    // {
+    //     std::cout << n << " ... " << fib2(n) << '\n';
+    // }
+    
     int x[] = {5, 3, 1, 6, 4, 2};
     bubblesort_pass(x, 0, 4);
     println(x, 6);
+    bubblesortsort(x, n - 2)
     return 0;
 }
