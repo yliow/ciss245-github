893d5f14ad5e234d58b5939f3a6e30c1764ac3ba
Yihsiang Liow <yliow@ccis.edu>
2023-12-07 15:34:56 -0500

    no msg

---
+++ 2023-12-07/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+class Constant
+{
+public:
+    virtual void print() const = 0;
+};
+
+class Int: public Constant
+{
+public:
+    Int(int v)
+        : v_(v)
+    {}
+    void print() const
+    {
+        std::cout << v_;
+    }
+    int v_;
+};
+
+class Double: public Constant
+{
+public:
+    Double(double v)
+        : v_(v)
+    {}
+    void print() const
+    {
+        std::cout << v_;
+    }
+    double v_;
+};
+
+int main()
+{
+    // want to iterate over {42, 3.14} and print the value
+    // Int x(42);
+    // x.print();
+    // std::cout << '\n';
+
+    // Double y(3.14);
+    // y.print();
+    // std::cout << '\n';
+
+    
+
+
+    
+    return 0;
+}
