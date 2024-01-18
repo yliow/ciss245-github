c3f926af27077c2c056a06bb9405dd43ac59dac8
Yihsiang Liow <yliow@ccis.edu>
2024-01-18 15:34:53 -0500

    no msg

---
+++ 2024-01-12/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int num_even_digits(int n);
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << num_even_digits(n) << '\n';
+    return 0;
+}
+
+
+int num_even_digits(int n)
+{
+    return 0;
+}
