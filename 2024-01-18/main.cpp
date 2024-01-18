364aa05596f47779f4bf2601b4ad9522b4418385
Yihsiang Liow <yliow@ccis.edu>
2024-01-18 15:55:23 -0500

    no msg

--- 2024-01-18/main.cpp
+++ 2024-01-18/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 
-int main()
+void max_subarray_sum(int x[], int n, int & max, int & max_i, int & max_j)
 {
-    int n = 5;
-    int x[1000] = {5, 3, -7, 2, -1};
-    int max = 0;
     for (int i = 0; i < n; ++i) // starting index of subarray
     {
         for (int j = i + 1; j <= n; ++j) // ending index of subarray
@@ -17,10 +14,24 @@ int main()
             if (s > max)
             {
                 max = s;
+                max_i = i;
+                max_j = j;
             }
-            std::cout << i << ' ' << j << ' ' << s << ' ' << max << '\n';
+            std::cout << i << ' ' << j << ' ' << s << ' '
+                      << max << ' ' << max_i << ' ' << max_j << '\n';
         }
     }
+}
+
+int main()
+{
+    int n = 5;
+    int x[1000] = {5, 3, -7, 2, -1};
+    int max = 0;
+    int max_i;
+    int max_j;
+    max_subarray_sum(x, n, max, max_i, max_j);
+    std::cout << "FINAL: " << max << ' ' << max_i << ' ' << max_j << '\n';
             
     return 0;
 }
