4c9ac9b959f1bb3d68f28ac9757f8eae143d73c6
Yihsiang Liow <yliow@ccis.edu>
2024-01-18 15:47:49 -0500

    no msg

--- 2024-01-18/main.cpp
+++ 2024-01-18/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     int n = 5;
     int x[1000] = {5, 3, -7, 2, -1};
+    int max = 0;
     for (int i = 0; i < n; ++i) // starting index of subarray
     {
         for (int j = i + 1; j <= n; ++j) // ending index of subarray
@@ -13,7 +14,11 @@ int main()
             {
                 s += x[k];
             }
-            std::cout << i << ' ' << j << ' ' << s << '\n';
+            if (s > max)
+            {
+                max = s;
+            }
+            std::cout << i << ' ' << j << ' ' << s << ' ' << max << '\n';
         }
     }
             
