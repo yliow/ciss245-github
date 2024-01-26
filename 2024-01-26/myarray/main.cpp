8b6ba8698aa62094e38a57912c2c2314b72ff1ee
Yihsiang Liow <yliow@ccis.edu>
2024-01-26 16:04:57 -0500

    no msg

--- 2024-01-26/myarray/main.cpp
+++ 2024-01-26/myarray/main.cpp
@@ -6,12 +6,12 @@ int main()
     int x[1000] = {};
     int x_size = 0;
     myarray_init(x, x_size); // set x_size to 0
-    myarray_pushback(x, x_size, 42);
-    myarray_println(x, x_size); // print {42}
-    myarray_pushback(x, x_size, 3);
-    myarray_println(x, x_size); // print {42, 3}
-    myarray_pushback(x, x_size, -1);
-    myarray_println(x, x_size); // print {42, 3, -1}
+    // myarray_pushback(x, x_size, 42);
+    // myarray_println(x, x_size); // print {42}
+    // myarray_pushback(x, x_size, 3);
+    // myarray_println(x, x_size); // print {42, 3}
+    // myarray_pushback(x, x_size, -1);
+    // myarray_println(x, x_size); // print {42, 3, -1}
 
     return 0;
 }
---
+++ 2024-01-26/myarray/myarray.h
@@ -0,0 +1,7 @@
+// myarray.h
+#ifndef MYARRAY_H
+#define MYARRAY_H
+
+myarray_init(int x[], int x_size);
+
+#endif
