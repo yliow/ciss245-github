5a7405db91f295af4f65c9ef94434b6fbfd6fbb1
Yihsiang Liow <yliow@ccis.edu>
2024-02-12 16:10:01 -0500

    no msg

---
+++ 2024-02-12/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+struct Time
+{
+    int hh;
+    int mm;
+    int ss;
+};
+
+void print(const Time & t)
+{
+    std::cout << t.hh << ':' << t.mm << ':' << t.ss;
+}
+
+void inc_ss(Time & t)
+{
+    std::cout << &t << '\n';
+    ++t.ss;
+}
+
+void inc_ss(Time * t)
+{
+    std::cout << &t << '\n';
+    //++t.ss;
+    //(*t).ss += 1;
+    t->ss += 1;
+}
+
+void operator+=(Time & t, int dss)
+{
+    t.ss += dss;
+}
+
+int main()
+{
+    Time now;
+    now.hh = 14;
+    now.mm = 46;
+    now.ss = 0;
+    print(now);
+    std::cout << &now << '\n';
+    inc_ss(now);
+    inc_ss(&now);
+    print(now);
+    for (int i = 0; i < 10; ++i)
+    {
+        now += 1;
+        std::cout << now;
+        std::cout << '\n';
+    }
+    return 0;
+}
