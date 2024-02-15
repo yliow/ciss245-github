981211782419a3417d657306bb65ebeead3dc39e
Yihsiang Liow <yliow@ccis.edu>
2024-02-15 15:30:16 -0500

    no msg

---
+++ 2024-02-15/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct vec2i
+{
+    int x, y;
+};
+
+struct Alien
+{
+    vec2i p;
+    vec2i v;
+    int energy;
+    int points;
+};
+
+void init(Alien & alien, int px, int py, int vx, int vy, int energy, int points)
+{
+    alien.p.x = px;
+    alien.p.x = px;
+    alien.v.x = vx;
+    alien.v.y = vy;
+    alien.energy = energy;
+    alien.points = points;
+}
+void run(Alien & alien)
+{
+    alien.p.x += alien.v.x;
+    alien.p.y += alien.v.y;
+}
+
+int main()
+{
+    Alien alien0;
+    init(alien, 10, 5, 1, 2, 50, 100);
+    run(alien);
+}
