a083eef4f542d41da3d88415fe4fb2d9013c433c
Yihsiang Liow <yliow@ccis.edu>
2024-02-29 15:30:14 -0500

    no msg

---
+++ 2024-02-29/Date.cpp
@@ -0,0 +1,77 @@
+// Date.cpp
+
+#include <iostream>
+#include "Date.h"
+
+Date::Date() // default constructor
+{
+    std::cout << "Date::Date() ...\n";
+}
+    
+Date::Date(int yyyy, int mm, int dd)
+    : yyyy_(yyyy), mm_(mm), dd_(dd)
+{
+    std::cout << "Date::Date(int,int,int) "
+              << yyyy << ' '
+              << mm << ' '
+              << dd << "...\n";
+}
+    
+void Date::init(int yyyy, int mm, int dd)
+{
+    yyyy_ = yyyy;
+    mm_ = mm;
+    dd_ = dd;
+}
+
+void Date::print()
+{
+    std::cout << yyyy_ << '-' << mm_ << '-' << dd_ << '\n';
+}
+
+void Date::add_y(int y)
+{
+    yyyy_ += y;
+}
+
+void Date::add_m(int m)
+{
+    mm_ += m;
+}
+
+void Date::add_d(int d)
+{
+    dd_ += d;
+}
+
+void Date::add_m_d(int m, int d)
+{
+    //mm_ += m;
+    //dd_ += d;
+
+    // another way:
+    add_m(m);
+    add_d(d);
+}
+
+void Date::add_y_m_d(int y, int m, int d)
+{
+    add_y(y);
+    add_m_d(m, d);
+}
+
+void Date::add_date(const Date & date)
+{
+    add_y_m_d(date.yyyy_, date.mm_, date.dd_);
+}
+
+bool Date::equals(const Date & date)
+{
+    return (yyyy_ == date.yyyy_ &&
+        mm_ == date.mm_ &&
+        dd_ == date.dd_);
+}
+bool Date::operator==(const Date & date)
+{
+    return equals(date);
+}
---
+++ 2024-02-29/Date.h
@@ -0,0 +1,30 @@
+// Date.h
+
+#ifndef DATE_H
+#define DATE_H
+
+class Date
+{
+  public:
+    // member functions = methods
+    void init(int, int, int);
+    Date(int, int, int);
+    Date();
+    void add_y(int);
+    void add_m(int);
+    void add_d(int);
+    void add_m_d(int, int);
+    void add_y_m_d(int, int, int);
+    void add_date(const Date &);
+    void print();
+    bool equals(const Date &);
+    bool operator==(const Date &);
+    
+  private:
+    int mm_;
+    int dd_;
+    int yyyy_;
+};
+
+
+#endif
