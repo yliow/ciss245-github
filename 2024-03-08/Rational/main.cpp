f939bac59d25d16b47284a57741d8210107c0335
Yihsiang Liow <yliow@ccis.edu>
2024-03-11 15:35:39 -0400

    no msg

---
+++ 2024-03-08/Rational/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Rational.h"
+
+int main()
+{
+    Rational r0(1, 2);
+    std::cout << "r0: " << r0 << '\n';
+    r0.set_n(100);
+    std::cout << r0.n() << '\n';
+    r0.n() = 2;
+    std::cout << "r0: " << r0 << '\n';
+    
+    Rational r1(2, 3);
+    std::cout << "r1: " << r1 << '\n';
+    r1 += r0; // r1.operator+=(r0)
+    std::cout << "r1: " << r1 << '\n';
+    
+    std::cout << r1 << '\n';
+    r1 = r1 + r0; // r1 + r0 ==== r1.operator+(r0)
+
+    return 0;
+}
