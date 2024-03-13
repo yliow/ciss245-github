99e75a2e62608863a971214d76d7f80555c89086
Yihsiang Liow <yliow@ccis.edu>
2024-03-13 15:35:14 -0400

    no msg

---
+++ 2024-03-13/IntDynArray/IntDynArray.h
@@ -0,0 +1,18 @@
+#ifndef INTDYNARRAY_H
+#define INTDYNARRAY_H
+
+class IntDynArray
+{
+  public:
+    IntDynArray(int);
+    int size() const;
+    int operator[](int) const;
+  private:
+    int * p_;
+    int size_;
+    int capacity_;
+};
+
+std::ostream & operator<<(std::ostream &, const IntDynArray &);
+
+#endif
