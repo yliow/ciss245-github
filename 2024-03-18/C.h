2e1794d70b6d78cd9f42ed8c971a19a0a1f54dcf
Yihsiang Liow <yliow@ccis.edu>
2024-03-18 19:57:45 -0400

    no msg

--- 2024-03-18/C.h
+++ 2024-03-18/C.h
@@ -6,6 +6,11 @@ class C
   public:
     C(int);
     int x;
+    static int get_y();
+    static void set_y(int);
+    static int get_z();
+    static void set_z(int);
+  private:
     static int y;
     static int z;
 };
--- 2024-03-18/main.cpp
+++ 2024-03-18/main.cpp
@@ -20,22 +20,23 @@ int main()
     std::cout << c42.x << '\n';
     std::cout << c99.x << '\n';
 
-    std::cout << c0.y << ' ' << c0.z << '\n';
-    std::cout << c42.y << ' ' << c42.z << '\n';
-    std::cout << c99.y << ' ' << c99.z << '\n';
-    c0.y = 567;
-    std::cout << c0.y << ' ' << c0.z << '\n';
-    std::cout << c42.y << ' ' << c42.z << '\n';
-    std::cout << c99.y << ' ' << c99.z << '\n';
+    std::cout << c0.get_y() << ' ' << c0.get_z() << '\n';
+    std::cout << c42.get_y() << ' ' << c42.get_z() << '\n';
+    std::cout << c99.get_y() << ' ' << c99.get_z() << '\n';
+    c0.set_y(567);
+    std::cout << c0.get_y() << ' ' << c0.get_z() << '\n';
+    std::cout << c42.get_y() << ' ' << c42.get_z() << '\n';
+    std::cout << c99.get_y() << ' ' << c99.get_z() << '\n';
 
-    std::cout << C::y << '\n';
-    std::cout << C::z << '\n';
-    C::z = 8888;
-    std::cout << C::y << '\n';
-    std::cout << C::z << '\n';
-    std::cout << c0.y << ' ' << c0.z << '\n';
-    std::cout << c42.y << ' ' << c42.z << '\n';
-    std::cout << c99.y << ' ' << c99.z << '\n';
+    std::cout << C::get_y() << '\n';
+    std::cout << C::get_z() << '\n';
+    //C::z = 8888;
+    C::set_z(8888);
+    std::cout << C::get_y() << '\n';
+    std::cout << C::get_z() << '\n';
+    std::cout << c0.get_y() << ' ' << c0.get_z() << '\n';
+    std::cout << c42.get_y() << ' ' << c42.get_z() << '\n';
+    std::cout << c99.get_y() << ' ' << c99.get_z() << '\n';
     
     return 0;
 }
