bca6e4e106e5dd50cec65da5f33bae3785119b19
Yihsiang Liow <yliow@ccis.edu>
2024-03-22 16:05:22 -0400

    no msg

---
+++ 2024-03-22/fib/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << i << ' ' << fib(i) << '\n';
+    }
+
+    return 0;
+}
