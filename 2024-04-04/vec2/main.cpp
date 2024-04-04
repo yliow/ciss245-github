f2848e9d1d26f642c0dd5596e143de81295eca8a
Yihsiang Liow <yliow@ccis.edu>
2024-04-04 15:48:06 -0400

    no msg

--- 2024-04-04/vec2/main.cpp
+++ 2024-04-04/vec2/main.cpp
@@ -4,10 +4,10 @@
 int main()
 {
     vec2< double > v0(0.1, 0.3);
-    //   std::cout << v0 << '\n';
+    std::cout << v0 << '\n';
     
     vec2< int > v1(1, 42);
-    //std::cout << v1 << '\n';
+    std::cout << v1 << '\n';
     
     return 0;
 }
--- 2024-04-04/vec2/vec2.h
+++ 2024-04-04/vec2/vec2.h
@@ -1,6 +1,8 @@
 #ifndef VEC2_H
 #define VEC2_D
 
+#include <iostream>
+
 template < typename T >
 class vec2
 {
@@ -8,8 +10,22 @@ class vec2
     vec2(T x, T y)
         : x_(x), y_(y)
     {}
+    T operator[](int i) const
+    {
+        return (i == 0 ? x_ : y_);
+    }
+    T operator[](int i)
+    {
+        return (i == 0 ? x_ : y_);
+    }
   private:
     T x_, y_;
 };
 
+template < typename T >
+std::ostream & operator<<(std::ostream & cout,
+                          const vec2< T > & v)
+{
+    return cout;
+}
 #endif
