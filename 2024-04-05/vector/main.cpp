b10c06f244627d8e5ce51bdc2e987952095e46d2
Yihsiang Liow <yliow@ccis.edu>
2024-04-05 15:48:38 -0400

    no msg

--- 2024-04-05/vector/main.cpp
+++ 2024-04-05/vector/main.cpp
@@ -4,13 +4,13 @@
 int main()
 {
     vector< int > v(3);
-    std::cout << v << '\n';
+    // std::cout << v << '\n';
 
-    for (int i = 42; i < 44; ++i)
-    {
-        v.push_back(i);
-        std::cout << v << '\n';
-    }
+    // for (int i = 42; i < 44; ++i)
+    // {
+    //     v.push_back(i);
+    //     std::cout << v << '\n';
+    // }
     
     return 0;
 }
