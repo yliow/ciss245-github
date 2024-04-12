1d8bf789f5eb98e3c97ddcf2251037793c32e19f
Yihsiang Liow <yliow@ccis.edu>
2024-04-12 15:52:21 -0400

    no msg

--- 2024-04-12/main.cpp
+++ 2024-04-12/main.cpp
@@ -15,9 +15,27 @@ std::ostream & operator<<(std::ostream & cout, const Int & i)
     return cout;
 }
 
+class Double
+{
+public:
+    Double(double i)
+        : value_(i)
+    {}
+    double value_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const Double & i)
+{
+    cout << "<Double " << i.value_ << '>';
+    return cout;
+}
+
 int main()
 {
     Int i(42);
     std::cout << i << '\n';
+    Double j(3.14);
+    std::cout << j << '\n';
+    
     return 0;
 }
