4d582c801e89a5193e904393f39c03eb5cb7c95c
Yihsiang Liow <yliow@ccis.edu>
2024-04-12 16:05:05 -0400

    no msg

--- 2024-04-12/main.cpp
+++ 2024-04-12/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <vector>
 
-class Int
+class Numeric
+{
+};
+
+class Int: public Numeric
 {
 public:
     Int(int i)
         : value_(i)
     {}
+    void print() const
+    {
+        std::cout << "<Int " << value_ << '>' << '\n';
+    }
     int value_;
 };
 
@@ -15,12 +24,17 @@ std::ostream & operator<<(std::ostream & cout, const Int & i)
     return cout;
 }
 
-class Double
+class Double: public Numeric
 {
 public:
     Double(double i)
         : value_(i)
     {}
+    void print() const
+    {
+        std::cout << "<Double " << value_ << '>' << '\n';
+    }
+
     double value_;
 };
 
@@ -36,6 +50,14 @@ int main()
     std::cout << i << '\n';
     Double j(3.14);
     std::cout << j << '\n';
-    
+
+    std::vector< Numeric * > x;
+    x.push_back(new Int(42));
+    x.push_back(new Double(2.718));
+    for (int i = 0; i < 2; ++i)
+    {
+        x[i]->print();
+    }
+    std::cout << '\n';
     return 0;
 }
