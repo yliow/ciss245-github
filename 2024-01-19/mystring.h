a1b540bdb81798761a657c2e89095b0e9fceb64f
Yihsiang Liow <yliow@ccis.edu>
2024-08-26 16:07:15 -0400

    no msg

--- 2024-01-19/mystring.h
+++
@@ -1,8 +0,0 @@
-// file: mystring.h
-
-#ifndef MYSTRING_H
-#define MYSTRING_H
-
-int mystrlen(char x[]);
-
-#endif
--- 2024-01-26/myarray/myarray.h
+++
@@ -1,9 +0,0 @@
-// myarray.h
-#ifndef MYARRAY_H
-#define MYARRAY_H
-
-void myarray_init(int x[], int & x_size);
-void myarray_println(int x[], int x_size);
-void myarray_pushback(int x[], int & x_size, int v);
-
-#endif
