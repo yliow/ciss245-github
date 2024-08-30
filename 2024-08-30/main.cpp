29f508973f931a93e86f2091599c977ce389d1d4
Yihsiang Liow <yliow@ccis.edu>
2024-08-30 16:14:37 -0400

    no msg

--- 2024-08-30/main.cpp
+++ 2024-08-30/main.cpp
@@ -31,16 +31,23 @@
 //     y = t;
 // }
 
-void swap(int & x, int & y)
+// void swap(int & x, int & y)
+// {
+//     int t = x;
+//     x = y;
+//     y = t;
+// }
+
+
+void print_fraction(int n, int d)
 {
-    int t = x;
-    x = y;
-    y = t;
+    std::cout << n << '/' << d;
 }
+
 
 int main()
 {
-    int x = 0;
+    // int x = 0;
     
     // std::cout << "main's x:" << x << '\n';
     // f(x);
@@ -49,10 +56,22 @@ int main()
     // inc(x);
     // std::cout << "x:" << x << '\n';
 
-    int y = 1;
-    std::cout << "x, y: " << x << ' ' << y << '\n';
-    swap(x, y);
-    std::cout << "x, y: " << x << ' ' << y << '\n';
-    
+    // int y = 1;
+    // std::cout << "x, y: " << x << ' ' << y << '\n';
+    // swap(x, y);
+    // std::cout << "x, y: " << x << ' ' << y << '\n';
+
+
+    print_fraction(1, 2);
+    std::cout << '\n';
+    print_fraction(3, 2);
+    std::cout << '\n';
+    print_fraction(0, 2);
+    std::cout << '\n';
+    print_fraction(-1, 2);
+    std::cout << '\n';    
+    print_fraction(-1, -2);
+    std::cout << '\n';
+
     return 0;
 }
