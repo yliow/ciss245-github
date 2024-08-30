104abf14b9d431eb671e9cb9236645cae590f798
Yihsiang Liow <yliow@ccis.edu>
2024-08-30 15:50:35 -0400

    no msg

--- 2024-08-30/main.cpp
+++ 2024-08-30/main.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
 
-void f(int x)
+// void f(int x)
+// {
+//     std::cout << "f's x:" << x << '\n';
+//     x = 42;
+//     std::cout << "f's x:" << x << '\n';
+// }
+
+// void f(int & x)
+// {
+//     std::cout << "f's x:" << x << '\n';
+//     x = 42;
+//     std::cout << "f's x:" << x << '\n';
+// }
+
+// void inc(int x)
+// {
+//     ++x;
+// }
+
+// void inc(int & x)
+// {
+//     ++x;
+// }
+
+void swap(int x, int y)
 {
-    std::cout << "f's x:" << x << '\n';
-    x = 42;
-    std::cout << "f's x:" << x << '\n';
+    int t = x;
+    x = y;
+    y = t;
 }
 
 int main()
 {
     int x = 0;
-    std::cout << "main's x:" << x << '\n';
-    f(x);
-    std::cout << "main's x:" << x << '\n';
+    
+    // std::cout << "main's x:" << x << '\n';
+    // f(x);
+    // std::cout << "main's x:" << x << '\n';
+
+    // inc(x);
+    // std::cout << "x:" << x << '\n';
+
+    int y = 1;
+    std::cout << "x, y: " << x << ' ' << y << '\n';
+    swap(x, y);
+    std::cout << "x, y: " << x << ' ' << y << '\n';
+    
+    return 0;
 }
