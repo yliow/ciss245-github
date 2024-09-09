420f67d1a06d827bb05d7fef1a40ea810c793d0c
Yihsiang Liow <yliow@ccis.edu>
2024-09-09 16:31:11 -0400

    no msg

---
+++ 2024-09-09/Fraction/Fraction.h
@@ -0,0 +1,14 @@
+// Fraction.h
+
+#ifndef FRACTION_H
+#define FRACTION_H
+
+#include "Fraction.h"
+
+int gcd(int a, int b);
+void print_fraction(int n, int d);
+void add_fraction(int xn, int xd,
+                  int yn, int yd,
+                  int & zn, int & zd);
+
+#endif
