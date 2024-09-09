767c3b54e93a46909774a18ac02751e615862fc8
Yihsiang Liow <yliow@ccis.edu>
2024-09-09 15:51:06 -0400

    no msg

---
+++ 2024-09-09/Array/Array.h
@@ -0,0 +1,11 @@
+// Array.h
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+void print_array(int x[], int start, int end);
+int linearsearch(int x[], int start, int end, int target);
+int linearsearch(int x[], int start, int end, int target);
+int max(int x[], int start, int end);
+
+#endif
---
+++ 2024-09-09/Array/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Array.h"
+
+int main()
+{
+    int x[5] = {2, 3, 5, 7};
+    print_array(x, 0, 5);
+    print_array(x, 0, 3);
+    print_array(x, 1, 4);
+    print_array(x, 1, 8);
+
+    std::cout << linearsearch(x, 0, 3, 2) << '\n';
+    std::cout << linearsearch(x, 0, 5, 7) << '\n';
+    std::cout << linearsearch(x, 0, 2, 7) << '\n';
+
+    std::cout << max(x, 0, 5) << '\n';
+    std::cout << max(x, 0, 3) << '\n';
+    
+    return 0;
+}
+
