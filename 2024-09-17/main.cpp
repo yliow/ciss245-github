87ff427dc2bb19d3e393974dcd427d19063d9d6d
Yihsiang Liow <yliow@ccis.edu>
2024-09-17 15:34:13 -0400

    no msg

---
+++ 2024-09-17/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 3.141 + 0.1 << '\n';
+    std::cout << 3.141 - 0.1 << '\n';
+    std::cout << 3.141 / 0.1 << '\n';
+    std::cout << 3.141 * 0.1 << '\n';
+    //std::cout << 3.141 % 0.1 << '\n';
+    
+    return 0;
+}
