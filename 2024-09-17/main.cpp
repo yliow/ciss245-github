c8a8c381e050b1fd21f5d8e2f6b405679c75dd4e
Yihsiang Liow <yliow@ccis.edu>
2024-09-17 15:43:15 -0400

    no msg

--- 2024-09-17/main.cpp
+++ 2024-09-17/main.cpp
@@ -6,7 +6,13 @@ int main()
     std::cout << 3.141 - 0.1 << '\n';
     std::cout << 3.141 / 0.1 << '\n';
     std::cout << 3.141 * 0.1 << '\n';
-    //std::cout << 3.141 % 0.1 << '\n';
+    //std::cout << 3.141 % 0.1 << '\n'; <<<< NO & for doublesx
+
+    std::cout << 3.141f + 0.1f << '\n';
+
+    double x, y;
+    std::cin >> x >> y;
+    std::cout << x << " + " << y << " = " << x + y << '\n';
     
     return 0;
 }
