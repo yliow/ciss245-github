e87cd94c4d71f157445b92df9758c953dd3113cd
Yihsiang Liow <yliow@ccis.edu>
2024-09-17 10:45:36 -0400

    no msg

--- 2024-09-16/mystring/mystring.cpp
+++ 2024-09-16/mystring/mystring.cpp
@@ -28,4 +28,43 @@ void mystrcpy(char x[], char y[])
         
         ++i;
     }
+
+    // FIRST CORRECTION
+    /*
+    int i = 0;
+    while (y[i] != '\0')
+    {
+        std::cout << "before ... "
+                  << i << ' '
+                  << x[i] << ' ' << int(x[i]) << ' '
+                  << y[i] << ' ' << int(y[i]) << '\n';
+        x[i] = y[i];
+        std::cout << "after ... "
+                  << i << ' '
+                  << x[i] << ' ' << int(x[i]) << ' '
+                  << y[i] << ' ' << int(y[i]) << '\n';
+        
+        ++i;
+    }
+    */
+    
+    // SECOND CORRECTION
+    /*
+    int i = 0;
+    while (y[i] != '\0')
+    {
+        std::cout << "before ... "
+                  << i << ' '
+                  << x[i] << ' ' << int(x[i]) << ' '
+                  << y[i] << ' ' << int(y[i]) << '\n';
+        x[i] = y[i];
+        std::cout << "after ... "
+                  << i << ' '
+                  << x[i] << ' ' << int(x[i]) << ' '
+                  << y[i] << ' ' << int(y[i]) << '\n';
+        
+        ++i;
+    }
+    x[i] = '\0';
+    */
 }
