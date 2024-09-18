889876b258ae177915f1e5109bd332c6005c9647
Yihsiang Liow <yliow@ccis.edu>
2024-09-18 16:17:17 -0400

    no msg

---
+++ 2024-09-18/mystring/mystring.h
@@ -0,0 +1,9 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+int mystrlen(char x[]);
+void mystrcpy(char x[], char y[]);
+void mystrcat(char s[], char t[]);
+int mystrcmp(char s[], char t[]);
+
+#endif
