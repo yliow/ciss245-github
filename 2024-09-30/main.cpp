bb4959d3ec52c0bd51a656ede51f85d4906fb180
Yihsiang Liow <yliow@ccis.edu>
2024-09-30 16:28:41 -0400

    no msg

--- 2024-09-30/main.cpp
+++ 2024-09-30/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+struct Fraction
+{
+    int n, d;
+};
+    
 int main()
 {
     int * p = NULL;
@@ -16,7 +21,41 @@ int main()
     }
 
     delete p;
-    std::cout << (*p) << '\n';
+    //std::cout << (*p) << '\n';
+
+    Fraction * q = NULL;
+    // ......
+    // ......
+    q = new Fraction;
+    // ......
+    delete q;
+    q = NULL;
+
+    p = new int[1000];
+    for (int i = 0; i < 1000; ++i)
+    {
+        *(p + i) = 42;
+        p[i] = 42;
+        *(p - i) = 42;
+    }
+    std::cout << (unsigned long long)(p) << '\n';
+    std::cout << (unsigned long long)(p + 1) << '\n';
+    std::cout << (unsigned long long)(p + 2) << '\n';
+
+    delete[] p;
+    p = NULL;
+
+    if (p == NULL)
+    {
+        p = new int[1000];
+    }
+
+    while (1)
+    {
+        std::cout << "hi\n":
+        p = new int[10000000];
+        //delete[] p;
+    }
     
     return 0;
 }
