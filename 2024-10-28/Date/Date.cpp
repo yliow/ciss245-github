b89bbb75f2c1e9ae604a38081ce3b2a5a3d9140d
Yihsiang Liow <yliow@ccis.edu>
2024-10-28 15:47:27 -0400

    no msg

---
+++ 2024-10-28/Date/Date.cpp
@@ -0,0 +1,74 @@
+// Date.cpp
+
+#include <iostream>
+#include "Date.h"
+
+Date::Date()
+    : yyyy_(1970), mm_(1), dd_(1)
+{}
+
+Date::Date(int y, int m, int d)
+    : yyyy_(y), mm_(m), dd_(d)
+{
+    //std::cout << "Date::Date with " << y << ' ' << m << ' '
+    //          << d << '\n';
+}
+
+void Date::init(int y, int m, int d)
+{
+    yyyy_ = y;
+    mm_ = m;
+    dd_ = d;
+}
+
+void Date::print() const
+{
+    std::cout << yyyy_ << '-' << mm_ << '-' << dd_ << '\n';
+}
+
+int Date::get_year() const
+{
+    return yyyy_;
+}
+
+int Date::get_month() const
+{
+    return mm_;
+}
+
+int Date::get_day() const
+{
+    return dd_;
+}
+
+std::ostream& operator<<(std::ostream & cout, const Date & date)
+{
+    cout << date.get_year() << '-'
+         << date.get_month() << '-'
+         << date.get_day();
+    return cout;
+}
+
+
+void Date::add_y(int y)
+{
+    yyyy_ += y;
+}
+
+void Date::add_m(int m)
+{
+    mm_ += m;
+}
+
+void Date::add_d(int d)
+{
+    dd_ += d;
+}
+
+void Date::add_y_m_d(int y, int m, int d)
+{
+    yyyy_ += y;
+    mm_ += m;
+    dd_ += d;
+}
+
