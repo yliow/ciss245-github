ef1c60671d2a8b5fec42c99f7fde3dcbc3f8923b
Yihsiang Liow <yliow@ccis.edu>
2024-10-30 15:58:14 -0400

    no msg

---
+++ 2024-10-30/Rational/Rational.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Rational.h"
+
+Rational::Rational()
+    : n_(0), d_(1)
+{}
+
+Rational::Rational(int n, int d)
+    : n_(n), d_(d)
+{}
+
+// Rational::Rational(const Rational & r)
+//     : n_(r.n_), d_(r.d_)
+// {}
+
+std::ostream & operator<<(std::ostream & cout, const Rational & r)
+{
+    cout << r.get_n() << '/' << r.get_d();
+    return cout;
+}
+
+int Rational::get_n() const
+{
+    return n_;        
+}
+
+int Rational::get_d() const
+{
+    return d_;        
+}
+
+void Rational::operator+=(const Rational & r)
+{
+    n_ = n_ * r.d_ + r.n_ * d_;
+    d_ *= r.d_;
+}
+
+
+
---
+++ 2024-10-30/Rational/Rational.h
@@ -0,0 +1,19 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+class Rational
+{
+  public:
+    Rational();
+    Rational(int, int);
+    //Rational(const Rational &);
+    int get_n() const;
+    int get_d() const;
+    void operator+=(const Rational &);
+  private:
+    int n_, d_;
+};
+
+std::ostream & operator<<(std::ostream &, const Rational&);
+
+#endif
