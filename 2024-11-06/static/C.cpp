5f06122daaa1c705bfcbb7e531bbfab3db8a1cbd
Yihsiang Liow <yliow@ccis.edu>
2024-11-06 16:07:00 -0500

    no msg

--- 2024-11-06/static/C.cpp
+++ 2024-11-06/static/C.cpp
@@ -3,3 +3,19 @@
 C::C(int x, int y)
     : x_(x), y_(y)
 {}
+
+std::ostream & operator<<(std::ostream & cout, const C & c)
+{
+    cout << "<C x:" << c.x() << ", y:" << c.y() << '>';
+    return cout;
+}
+
+int C::x() const
+{
+    return x_;
+}
+
+int C::y() const
+{
+    return y_;
+}
--- 2024-11-06/static/C.h
+++ 2024-11-06/static/C.h
@@ -1,13 +1,20 @@
 #ifndef C_H
 #define C_H
 
+#include <iostream>
+
 class C
 {
   public:
     C(int, int);
+    int x() const;
+    int y() const;
   private:
     int x_;
     int y_;
 };
+
+std::ostream & operator<<(std::ostream &, const C &);
+
 
 #endif
