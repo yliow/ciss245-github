6597adc70441672262b8cf569bed31cc5b5bce82
Yihsiang Liow <yliow@ccis.edu>
2024-11-08 15:57:30 -0500

    no msg

--- 2024-11-08/static/main.cpp
+++ 2024-11-08/static/main.cpp
@@ -15,6 +15,12 @@ int main()
     C b(3);
     std::cout << "b:" << b << '\n';
     std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+    C c(3);
+    std::cout << "c:" << c << '\n';
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+    C d(3);
+    std::cout << "d:" << d << '\n';
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
 
     //std::cout << C::y_ << '\n';
     std::cout << "C::y_:" << C::get_y() << '\n';
