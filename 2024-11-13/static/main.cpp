c82d740874d6cbfeed38d385f9456e99386bb878
Yihsiang Liow <yliow@ccis.edu>
2024-11-13 18:42:01 -0500

    no msg

---
+++ 2024-11-13/static/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "C.h"
+
+int main()
+{
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+    if (1)
+    {
+        C a(1);
+        std::cout << "a:" << a << '\n';
+        std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+    }
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+
+    C b(3);
+    std::cout << "b:" << b << '\n';
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+    C c(3);
+    std::cout << "c:" << c << '\n';
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+    C d(3);
+    std::cout << "d:" << d << '\n';
+    std::cout << "C::num_objects_:" << C::get_num_objects() << '\n';
+
+    //std::cout << C::y_ << '\n';
+    std::cout << "C::y_:" << C::get_y() << '\n';
+    
+    return 0;
+}
