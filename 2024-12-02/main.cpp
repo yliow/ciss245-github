9f21601b046183118b0fdaf1359ebba14f8b126b
Yihsiang Liow <yliow@ccis.edu>
2024-12-02 16:05:04 -0500

    no msg

--- 2024-12-02/main.cpp
+++ 2024-12-02/main.cpp
@@ -26,10 +26,38 @@ int g(int x)
     }
 }
 
+// recursive sum from 1 to n
+int sum0(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum0(n - 1) + n;
+    }
+}
+
+// iterative version of sum from 1 to n
+int sum1(int n)
+{
+    int ret = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        ret += i;
+    }
+    return ret;
+}
 
 int main()
 {
     //std::cout << f(4) << '\n';
-    std::cout << g(3) << '\n';
+    //std::cout << g(3) << '\n';
+    for (int n = 0; n < 10; ++n)
+    {
+        std::cout << sum0(n) << ' ';
+        std::cout << sum1(n) << '\n';
+    }
     return 0;
 }
