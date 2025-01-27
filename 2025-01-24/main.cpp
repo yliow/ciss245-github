629def2593d5b6d770e508e04ec3d3f957056182
Yihsiang Liow <yliow@ccis.edu>
2025-01-27 15:33:39 -0500

    no msg

---
+++ 2025-01-24/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// inline
+// int max(int x, int y)
+// {
+//     return (x >= y ? x : y);
+// }
+
+int main()
+{
+    // std::cout << max(2, 3) << '\n';
+    std::cout << (2 >= 3 ? 2 : 3) << '\n';
+    
+    return 0;
+}
