efb181d860b53e41935eba9526df1b69c8d7aa94
Yihsiang Liow <yliow@ccis.edu>
2025-02-20 15:10:48 -0500

    no msg

---
+++ 2025-02-20/Date/Date.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Date.h"
+
+void init(Date & date, int y, int m, int d)
+{
+    date.yyyy_ = y;
+    date.mm_ = m;
+    date.dd_ = d;
+}
+
+void print(const Date & date)
+{
+    std::cout << date.yyyy_ << '/'
+              << date.mm_ << '/'
+              << date.dd_ << '\n';
+}
+
+// void add_y(Date &, int)
+// {
+// }
+
+// void add_m(Date &, int)
+// {
+// }
+
+// void add_d(Date &, int)
+// {
+// }
--- 2025-02-20/Date/main.cpp
+++ 2025-02-20/Date/main.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
+    Date today, yesterday;
+    init(today, 2025, 2, 20);
+    print(today);
+    
+    init(yesterday, 2025, 2, 19);
+    print(yesterday);
+    
     return 0;
 }
