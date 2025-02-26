f814b60dab34e2c667522807a7d144c517a70543
Yihsiang Liow <yliow@ccis.edu>
2025-02-26 16:35:32 -0500

    no msg

---
+++ 2025-02-26/Date/Date.h
@@ -0,0 +1,30 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+
+class Date
+{
+  public:
+    Date();             // default constructor of Date
+    Date(const Date &); // copy constructor of Date
+    Date(int, int, int);
+    //void init(int, int, int);
+    void print() const;
+    void add_y(int); // Date::add_y
+    void add_m(int);
+    void add_d(int);
+    void add_m_d(int, int);
+    bool equal(const Date &) const;
+    bool operator==(const Date &) const;
+    int get_year() const;
+    int get_month() const;
+    int get_day() const;
+    const Date & operator=(const Date &);
+private:
+    int yyyy_, mm_, dd_;
+};
+
+std::ostream & operator<<(std::ostream &, const Date &);
+
+#endif
