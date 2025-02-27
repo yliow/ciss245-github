172a6ba98db02047cd1ee4d07ef7efd50b0b3579
Yihsiang Liow <yliow@ccis.edu>
2025-02-27 15:55:28 -0500

    no msg

--- 2025-02-27/vec2d/vec2d.cpp
+++ 2025-02-27/vec2d/vec2d.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-#incliude "vec2d.h"
+#include "vec2d.h"
 
 vec2d::vec2d(double x, double y)
     : x_(x), y_(y)
 {}
 
+double vec2d::get_x() const
+{
+    return x_;
+}
+
+double vec2d::get_y() const
+{
+    return y_;
+}
+
 std::ostream & operator<<(std::ostream & cout, const vec2d & v)
 {
-    cout << '<' << v.x_ << ", " << v.y_ << '>';
+    cout << '<' << v.get_x() << ", " << v.get_y() << '>';
     return cout;
 }
--- 2025-02-27/vec2d/vec2d.h
+++ 2025-02-27/vec2d/vec2d.h
@@ -7,8 +7,12 @@ class vec2d
 {
   public:
     vec2d(double, double);
+    double get_x() const;
+    double get_y() const;
   private:
     double x_, y_;
 };
+
+std::ostream & operator<<(std::ostream &, const vec2d &);
 
 #endif
