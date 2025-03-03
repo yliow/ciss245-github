5b5a5d0f60df5a11fe7a2c161dba386aea063261
Yihsiang Liow <yliow@ccis.edu>
2025-03-03 15:45:52 -0500

    no msg

---
+++ 2025-03-03/vec2d/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "vec2d.h"
+
+int main()
+{
+    vec2d v(1.1, 2.2);
+    std::cout << "v:" << v << '\n';
+
+    std::cout << "v[0]:" << v[0] << '\n'; // v[0] = v.operator[](0) return VALUE of v.x_
+    v[0] = 4.4; // same as v[0] = v.operator[](0) return REFERENCE to v.x_
+    std::cout << v << '\n';
+
+    v *= 2.5; // v.operator*=(2.5)
+    std::cout << v << '\n';
+    std::cout << v * 2.5 << '\n'; // v * 2.5 same as
+                                  // v.operator*(2.5)
+
+    v[0] = 0;
+    v[1] = 1;
+    vec2d u(0.1, 0.2);
+    std::cout << (u += v) << '\n';
+    std::cout << u + v << '\n';;
+
+    vec2d w;
+    std::cout << "w:" << w << '\n';
+    
+    return 0;
+}
---
+++ 2025-03-03/vec2d/vec2d.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "vec2d.h"
+
+vec2d::vec2d()
+    : x_(0), y_(0)
+{}
+
+vec2d::vec2d(double x, double y)
+    : x_(x), y_(y)
+{}
+
+double vec2d::get_x() const
+{
+    return x_;
+}
+
+double vec2d::get_y() const
+{
+    return y_;
+}
+
+double vec2d::operator[](int i) const
+{
+    if (i == 0) return x_;
+    else if (i == 1) return y_;
+    else return 99999; // WRONG ... need exception
+}
+double & vec2d::operator[](int i)
+{
+    if (i == 0) return x_;
+    else if (i == 1) return y_;
+    else return x_; // WRONG .. need exception
+}
+
+vec2d & vec2d::operator*=(double d)
+{
+    x_ *= d;
+    y_ *= d;
+    return (*this);
+}
+
+vec2d vec2d::operator*(double d)
+{
+    return (vec2d(*this) *= d);
+}
+
+vec2d & vec2d::operator+=(const vec2d & v)
+{
+    x_ += v.x_;
+    y_ += v.y_;
+    return (*this);
+}
+
+vec2d vec2d::operator+(const vec2d & v) const
+{
+    return (vec2d(*this) += v);
+}
+
+
+std::ostream & operator<<(std::ostream & cout, const vec2d & v)
+{
+    cout << '<' << v.get_x() << ", " << v.get_y() << '>';
+    return cout;
+}
