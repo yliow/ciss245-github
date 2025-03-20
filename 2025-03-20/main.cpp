d1abbf67064ac765ae9f7ef96982e55f79f96c0d
Yihsiang Liow <yliow@ccis.edu>
2025-03-20 15:41:20 -0400

    no msg

--- 2025-03-20/main.cpp
+++ 2025-03-20/main.cpp
@@ -17,15 +17,24 @@ int c(int n, int r)
     }
 }
 
-long long int fib(int n)
+long long int fib(int n, long long int table[])
 {
     if (n == 0 || n == 1)
     {
+        std::cout << "base case ... " << n << " return 1\n";
         return 1;
     }
     else
     {
-        return fib(n - 1) + fib(n - 2);
+        std::cout << "recursive case ... " << n << '\n';
+        if (table[n] == 0)
+        {
+            long long int ret = fib(n - 1, table) + fib(n - 2, table);
+            table[n] = ret;
+        }
+        std::cout << "recursive case ... " << n << " return " << table[n]
+                  << '\n';
+        return table[n];
     }
 }
 
@@ -34,9 +43,11 @@ int main()
     //std::cout << c(5, 2) << '\n';
     //std::cout << c(5, 1) << '\n';
 
-    for (int n = 0; n <= 50; ++n)
+    long long int table[100] = {1, 1, 0};
+    
+    for (int n = 0 ; n <= 50; ++n)
     {
-        std::cout << n << ' ' << fib(n) << '\n';
+        std::cout << n << ' ' << fib(n, table) << '\n';
     }
 
     
