166e77a9c5ec3a2aaf4be343b1b48ffc9f581702
Yihsiang Liow <yliow@ccis.edu>
2025-03-20 15:25:57 -0400

    no msg

--- 2025-03-20/main.cpp
+++ 2025-03-20/main.cpp
@@ -4,16 +4,41 @@ int c(int n, int r)
 {
     if (r == 0 || n == r)
     {
+        std::cout << "base case ... " << n << ", " << r << " return 1\n";
         return 1;
     }
     else
     {
-        return c(n - 1, r) + c(n - 1, r - 1);
+        std::cout << "recursive case ... " << n << ", " << r << '\n';
+        int ret = c(n - 1, r) + c(n - 1, r - 1);
+        std::cout << "recursive case ... " << n << ", " << r
+                  << " return " << ret << '\n';
+        return ret;
+    }
+}
+
+long long int fib(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(n - 1) + fib(n - 2);
     }
 }
 
 int main()
 {
-    std::cout << c(5, 2) << '\n';
+    //std::cout << c(5, 2) << '\n';
+    //std::cout << c(5, 1) << '\n';
+
+    for (int n = 0; n <= 50; ++n)
+    {
+        std::cout << n << ' ' << fib(n) << '\n';
+    }
+
+    
     return 0;
 }
