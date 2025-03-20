4f431c02b3a0e0dd9e76341bcdb2bb2362c94b39
Yihsiang Liow <yliow@ccis.edu>
2025-03-20 15:04:27 -0400

    no msg

---
+++ 2025-03-20/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int c(int n, int r)
+{
+    if (r == 0 || n == r)
+    {
+        return 1;
+    }
+    else
+    {
+        return c(n - 1, r) + c(n - 1, r - 1);
+    }
+}
+
+int main()
+{
+    std::cout << c(5, 2) << '\n';
+    return 0;
+}
