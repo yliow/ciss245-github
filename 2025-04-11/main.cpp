e5dd0492e90108b429eeb8f738c874bf72969dc7
Yihsiang Liow <yliow@ccis.edu>
2025-04-11 16:02:54 -0400

    no msg

--- 2025-04-11/main.cpp
+++ 2025-04-11/main.cpp
@@ -24,24 +24,39 @@ std::ostream & operator<<(std::ostream & cout, const Man & man)
     cout << "<Man " << man.get_name() << ", " << man.get_x() << '>';
     return cout;
 };
-//class Dog
-//{
-//public:
-//    Dog(std::string name, int )
-//        : name_(name), x_(x)
-//    {}
-//private:
-//    std::string name_;
-//    int x_;
-//};
-//
+
+class Dog
+{
+public:
+    Dog(std::string name, int )
+        : name_(name), x_(x)
+    {}
+    std::string get_name() const
+    {
+        return name_;
+    }
+    int get_x() const
+    {
+        return x_;
+    }
+private:
+    std::string name_;
+    int x_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const Dog & dog)
+{
+    cout << "<Dog " << dog.get_name() << ", " << dog.get_x() << '>';
+    return cout;
+};
 
 int main()
 {
     Man john("John", 5);
-    //Dog rex(...);
+    Dog rex("Rex", 8);
 
     std::cout << john << '\n';
+    std::Cout << rex << '\n';
     return 0;
     
 }
