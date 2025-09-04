b1b173fe57c7e742560b271963580c418ecfd695
Yihsiang Liow <yliow@ccis.edu>
2025-09-04 16:13:25 -0400

    no msg

---
+++ 2025-09-04/array/array.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "array.h"
+
+// notation: x[2],x[3],x[4] ... x[2..4]
+// bubblesort of x[0 .. n - 1]
+//
+// for i = n - 2, n - 3, ..., 0:
+//     for j = 0, ..., i
+//         if x[j] > x[j + 1]:
+//             swap x[j] and x[j + 1]
+void bubblesort(int x[], int start, int end)
+{
+    for (int i = end - 2; i >= start; --i)
+    {
+        for (int j= start; j <= i; ++j)
+        {
+            if (x[j] > x[j + 1])
+            {
+                int t = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = t;
+            }
+        }
+    }
+}
+
+/*
+  lower = 0
+  upper = n - 1
+  while (lower <= upper)
+  {
+      mid = (lower + upper) / 2 ... mid = lower + (upper - lower) / 2
+      if (x[mid] == target)
+         return mid
+      else if (target < x[mid]):
+         upper = mid - 1
+      else
+         lower = mid + 1
+  }
+  return -1;
+ */
+int binarysrch(int x[], int start, int end, int target)
+{
+}
+
+int linearsrch(int x[], int start, int end, int target)
+{
+    for (int i = start; i < end; ++i)
+    {
+        if (x[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void print(int x[], int start, int end)
+{
+    std::cout << '{';
+    for (int i = start; i < end; ++i)
+    {
+        std::cout << x[i];
+        if (i < end - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << '}';
+}
+
+void println(int x[], int start, int end)
+{
+    print(x, start, end);
+    std::cout << '\n';
+}
