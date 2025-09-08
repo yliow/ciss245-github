c606c37302d0c277bdb87878a5416f55d7719d5d
Yihsiang Liow <yliow@ccis.edu>
2025-09-08 15:53:14 -0400

    no msg

--- 2025-09-08/fraction/main.cpp
+++ 2025-09-08/fraction/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include "array.h"
+#include "fraction.h"
 
-bool isprime(int n);
-void add_fraction(int n0, int d0,
-                  int n1, int d1,
-                  int & n2, int & d2);
 
 int main()
 {
@@ -16,67 +12,12 @@ int main()
     int n2, d2;
     add_fraction(n0, d0, n1, d1, n2, d2);
 
-    std::cout << n0 << '/' << d0
-              << " + "
-              << n1 << '/' << d1
-              << " = "
-              << n2 << '/' << d2
-              << '\n';
-     return 0;
+    print_fraction(n0, d0);
+    std::cout << " + ";
+    print_fraction(n1, d1);
+    std::cout << " = ";
+    print_fraction(n2, d2);
+    std::cout << '\n';
+    return 0;
 }
 
-
-int min(int x, int y)
-{
-    return (x <= y ? x : y);
-}
-
-
-int GCD(int m, int n)
-{
-    // now compute GCD of m,n. store it in g.
-    int g = 1;
-    for (int i = min(m, n); i >= 1; --i)
-    {
-        if (m % i == 0 && n % i == 0)
-        {
-            g = i;
-            break;
-        }
-    }
-    return g;
-}
-
-void add_fraction(int n0, int d0,
-                  int n1, int d1,
-                  int & n2, int & d2)
-{
-    // compute n2/d2 as n0/d0 + n1/d1.
-    n2 = n0 *d1 + d0 * n1;
-    d2 = d0 * d1;
-
-    // simplify the fraction n2/d2
-    int g = GCD(n2, d2);
-    n2 /= g;
-    d2 /= g;
-}
-
-
-bool isprime(int n)
-{
-    if (n <= 1)
-    {
-        return false;
-    }
-    else
-    {
-        for (int d = 2; d <= sqrt(n); ++d)
-        {
-            if (n % d == 0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-}
