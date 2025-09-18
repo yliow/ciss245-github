ab4f21cca4afa4ac3c8238b56b02d821b25bba16
Yihsiang Liow <yliow@ccis.edu>
2025-09-18 15:27:24 -0400

    no msg

---
+++ 2025-09-18/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// void f1(int x)
+// {
+//     x = 0;
+// }
+
+// void f0(int & r)
+// {
+//     r = 0;
+//     return;
+// }
+
+// void f2(int * p)
+// {
+//     *p = 0;
+// }
+
+int main()
+{
+    // int x = 42;
+    // f0(x);
+    // std::cout << x << '\n';
+
+    // x = 42;
+    // f1(x);
+    // std::cout << x << '\n';
+
+    // x = 42;
+    // f2(&x);
+    // std::cout<< x << '\n';
+    
+    return 0;
+}
