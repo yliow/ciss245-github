fe44371ff4b4a5e0124ae90dc05f07fc7f8ec880
Yihsiang Liow <yliow@ccis.edu>
2025-09-19 15:29:11 -0400

    no msg

--- 2025-09-18/main.cpp
+++ 2025-09-18/main.cpp
@@ -16,6 +16,30 @@
 //     *p = 0;
 // }
 
+// void f0(int x)
+// {
+//     ++x;
+// }
+
+// void f1(int & r)
+// {
+//     ++r;
+// }
+
+// void f2(int * p)
+// {
+//     ++(*p);
+// }
+
+bool isprime(int n)
+{
+}
+
+void nextprime(int & x)
+{
+}
+
+    
 int main()
 {
     // int x = 42;
@@ -29,6 +53,36 @@ int main()
     // x = 42;
     // f2(&x);
     // std::cout<< x << '\n';
-    
+
+    // int x = 0;
+    // f0(x);
+    // std::cout << x << '\n';
+
+    // x = 0;
+    // f1(x);
+    // std::cout << x << '\n';
+
+    // x = 0;
+    // f2(&x);
+    // std::cout << x << '\n';
+
+    // int x = 0, y = 1;
+    // swap0(x, y);
+    // std::cout << x << ' ' << y << '\n'; // fails... 0 1
+
+    // x = 0; y = 1;
+    // swap1(x, y);
+    // std::cout << x << ' ' << y << '\n'; // WORKS ... 1 0
+
+    // x = 0; y = 1;
+    // swap2(&x, &y);
+    // std::cout << x << ' ' << y << '\n'; // WORKS ... 1 0
+
+    int x = 7;
+
+    nextprime(x);
+    std::cout << x << '\n'; // should print 11
+
+
     return 0;
 }
