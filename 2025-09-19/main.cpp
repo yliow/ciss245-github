22a60bd9497f2c0dbf0613f30052f6498c146604
Yihsiang Liow <yliow@ccis.edu>
2025-09-19 15:38:38 -0400

    no msg

---
+++ 2025-09-19/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+bool isprime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    else
+    {
+        for (int d = 2; d <= sqrt(n); ++d)
+        {
+            if (n % d == 0) return false;
+        }
+        return true;
+    }
+}
+
+void nextprime(int & x)
+{
+    ++x;
+    while (!isprime(x))
+    {
+        ++x;
+    }
+    return;
+}
+
+    
+int main()
+{
+    // for (int i = 1; i <= 10; ++i)
+    // {
+    //     std::cout << i << ' ' << isprime(i) << '\n';
+    //}
+    
+    int x = 7;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        nextprime(x);
+        std::cout << x << '\n';
+    }
+
+
+    return 0;
+}
