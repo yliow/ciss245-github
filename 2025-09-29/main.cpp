21e83d3d5991cecdeac0fff6db7f2cb24f6ad767
Yihsiang Liow <yliow@ccis.edu>
2025-10-01 12:35:52 -0700

    no msg

---
+++ 2025-09-29/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+/*
+
+HEAP
+    give int at 5000
+
+
+
+
+ */
+void f()
+{
+    int * p = new int[1000000];
+}
+
+int main()
+{
+    while (1)
+    {
+        f();
+    }
+    
+    return 0;
+}
