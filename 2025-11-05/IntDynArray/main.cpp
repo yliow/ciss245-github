09d189cbcf558cf8a3b413d5c949b38047aa1ea7
Yihsiang Liow <yliow@ccis.edu>
2025-11-05 13:31:55 -0800

    no msg

---
+++ 2025-11-05/IntDynArray/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    IntDynArray a(5); // a models array size 0 within an array of capacity 5;
+    IntDynArray b(1000); // b.capacity_ is 1000, b.size_ = 0
+    a.resize(3);
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 10;
+    std::cout << a << '\n'; // prints {1, 2, 10}
+    a = b; // everything in b transferred to a
+    a.push_back(8); // add 8 to "back" of array, expanded automaticaly if nec.
+
+    // a has a.p_ pointer to an array on the heap
+    // a has a.size_
+    // a has a.capacity_
+}
