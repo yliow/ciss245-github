c2340e43be4df1000fa21520a0d1f78456db9927
Yihsiang Liow <yliow@ccis.edu>
2025-11-06 12:33:50 -0800

    no msg

---
+++ 2025-11-06/IntDynArray/IntDynArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "IntDynArray.h"
+
+IntDynArray::IntDynArray(int capacity)
+    : p_(new int[capacity]), size_(0), capacity_(capacity)
+{}
+
+
+int IntDynArray::size() const
+{
+    return size_;
+}
+
+
+int IntDynArray::operator[](int i) const
+{
+    return a.p_[i];
+}
+
+
+int & IntDynArray::operator[](int i)
+{
+    return a.p_[i];
+}
+
+
+std::ostream & operator<<(std::ostream & cout, const IntDynArray & a)
+{
+    cout << '{';
+    for (int i = 0; i < a.size(); ++i)
+    {
+        cout << a[i];
+        if (i < a.size() - 1) cout << ", ";
+    }
+    cout << '}';
+    return cout;
+}
---
+++ 2025-11-06/IntDynArray/IntDynArray.h
@@ -0,0 +1,22 @@
+#ifndef INTDYNARRAY_H
+#define INTDYNARRAY_H
+
+class IntDynArray
+{
+  public:
+    IntDynArray(int); // input is capacity request
+    ~IntDynArray();
+    IntDynArray(const IntDynArray &);
+    IntDynArray & operator(const IntDynArray &);
+    int size() const;
+    int operator[](int) const;
+    int & operator[](int);
+  private:
+    int p_;
+    int size_;
+    int capacity_;
+};
+
+std::ostream & operator<<(std::ostream &, const IntDynArray &);
+
+#endif
