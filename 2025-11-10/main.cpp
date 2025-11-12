01ffb1b3d09e9415c328d8e9da08980478cd4e67
Yihsiang Liow <yliow@ccis.edu>
2025-11-12 12:34:32 -0800

    no msg

--- 2025-11-10/main.cpp
+++ 2025-11-10/main.cpp
@@ -1,134 +1,64 @@
 #include <iostream>
 
-// void println(int x)
-// {
-//     std::cout << x << '\n';
-// }
-
-// void println(double x)
-// {
-//     std::cout << x << '\n';
-// }
-
-// void println(char x)
-// {
-//     std::cout << x << '\n';
-// }
-
-template < typename T >
-void println(T x)
+int f(int x)
 {
-    std::cout << x << '\n';
+    std::cout << "entering f() with x = " << x << '\n';
+    // if (x == 0)
+    // {
+    //     return 0;
+    // }
+    // else
+    // {
+    //     return f(x - 1);
+    // }
+    return f(x - 1);
 }
 
-// void swap(int & x, int & y)
-// {
-//     int t = x;
-//     x = y;
-//     y = t;
-// }
-// void swap(double & x, double & y)
-// {
-//     double t = x;
-//     x = y;
-//     y = t;
-// }
-
-template < typename S >
-void swap(S & x, S & y)
+//sum from 0 to n
+int sum0(int n) // iterative version
 {
-    S t = x;
-    x = y;
-    y = t;
-}
-
-template < typename T >
-void bubblesort(T x[], int n)
-{
-    for (int i = n - 2; i >= 0; --i)
+    int s = 0;
+    for (int i = 0; i <= n; ++i)
     {
-        for (int j = 0; j <= i; ++j)
-        {
-            if (x[j] > x[j + 1])
-            {
-                swap< T >(x[j], x[j + 1]);
-            }
-        }
+        s += i;
+    }
+    return s;
+}
+
+int sum1(int n) // recursive version
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum1(n - 1) + n;
     }
 }
 
-// try all the array functions
-
-// struct vec2d
-// {
-//     double x, y;
-// };
-// void println(const vec2d & v)
-// {
-//     std::cout << '<' << v.x << ", " << v.y << '>' << '\n';
-// }
-
-// struct vec2i
-// {
-//     int x, y;
-// };
-// void println(const vec2i & v)
-// {
-//     std::cout << '<' << v.x << ", " << v.y << '>' << '\n';
-// }
-
-template < typename T >
-struct vec2
+int sumsq(int n)
 {
-    T x, y;
-};
-
-template < typename T >
-void println(const vec2< T > & v)
-{
-    std::cout << '<' << v.x << ", " << v.y << '>' << '\n';
+    if (n == 0) return 0;
+    else return sumsq(n - 1) + n * n;
 }
 
-template < typename T >
-vec2< T > operator+(const vec2< T > & u, const vec2< T > & v)
+int twopower(int n)
 {
-    vec2< T > ret = {u.x + v.x, u.y + v.y};
-    return ret;
+    if (n == 0) return 1;
+    else return twopower(n - 1) * 2;
 }
 
 int main()
 {
-    vec2< double > v = {1.1, 2.5};
-    vec2< int > u = {2, 5};
-    println< double >(v);
-    println< int >(u);
-    
-    // int i = 42;
-    // typedef int INTEGER;
-    // INTEGER j = 42;
+    // int x = f(3);
+    // std::cout << "main() ... x = " << x << '\n';
 
-    // typedef int CARD; // 1 - 1, 2 - 2, ..., 9 - 9, J - 10, Q - 11, K - 12
-    // CARD card0 = 5;
-
-    // char x[4][10][10];
-    // typedef char tetris_shape [4][10][10];
-    // tetrais_shape T = {......};
-
-    println< int >(3);
-    println(4);
-    println< double >(3.1415);
-    println(2.417);
-    println< char >('$');
-    println('A');
-
-    int x = 0, y = 1;
-    swap< int >(x, y);
-    std::cout << x << ' ' << y << '\n';
-    double a = 0.1, b = 0.2;
-    swap< double >(a, b);
-    std::cout << a << ' ' << b << '\n';
-    swap(a, b);
-    std::cout << a << ' ' << b << '\n';
+    for (int n = 0; n < 9; ++n)
+    {
+        std::cout << n << ' ' << sum0(n) << ' ' << sum1(n)
+                  << '\n';
+    }
     
     return 0;
 }
