23db321cf162458c328b7744795d625f3184bdcf
Yihsiang Liow <yliow@ccis.edu>
2025-11-21 14:08:59 -0800

    no msg

---
+++ 2025-11-21/inher/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class GameObject
+{
+public:
+    GameObject(int x, int y)
+        : x_(x), y_(y)
+    {}
+    int get_x() { return x_; }
+    int get_y() { return y_; }
+    
+private:
+    int x_, y_;
+};
+
+class Alien: public GameObject
+{
+public:
+    Alien(int x, int y, int points)
+        : GameObject(x, y), points_(points)
+    {}
+private:
+    int points_;
+};
+
+class Spaceship
+{
+    Spaceship(int x, int y)
+        : x_(x), y_(y)
+    {}
+private:
+};
+
+int main()
+{
+    Alien a(5, 6, 10);
+    Spaceship(10, 10);
+
+    return 0;
+}
