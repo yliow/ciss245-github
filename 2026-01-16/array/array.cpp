0502c787e72d11868cf7f34ed27b5f09690ea7e4
Yihsiang Liow <yliow@ccis.edu>
2026-01-16 12:31:26 -0800

    no msg

---
+++ 2026-01-16/array/array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "array.h"
+
+void array_print(int x[], int x_size, int x_capacity)
+{
+    std::cout << '{';
+    for (int i = 0; i < x_size; ++i)
+    {
+        std::cout << x[i];
+        if (i < x_size - 1) std::cout << ", ";
+    }
+    std::cout << '}';
+}
+
+void array_println(int x[], int x_size, int x_capacity)
+{
+    array_print(x, x_size, x_capacity);
+    std::cout << '\n';
+}
+
+void push_back(int x[], int & x_size, int x_capacity, int e)
+{
+    if (x_size < x_capacity)
+    {
+        x[x_size] = e;
+        ++x_size;
+    }
+}
---
+++ 2026-01-16/array/array.h
@@ -0,0 +1,8 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+void array_print(int [], int, int);
+void array_println(int [], int, int);
+void push_back(int [], int &, int, int);
+
+#endif
