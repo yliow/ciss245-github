9bb30c14cd9bbc1a49ee8f324570a81c1fc121d4
Yihsiang Liow <yliow@ccis.edu>
2026-01-22 11:45:34 -0800

    no msg

--- 2026-01-21/main.cpp
+++ 2026-01-21/main.cpp
@@ -8,18 +8,22 @@ void copy(int x[], int t[], int size)
     }
 }
 
-void print(int x, int size)
+void print(int x[], int size)
 {
+    std::cout << '|';
     for (int i = 0; i < size; ++i)
     {
         std::cout << (x[i] == 0 ? ' ' : 'X');
     }
+    std::cout << '|';
 }
 
 void update(int x[], int n)
 {
     int bits[8] = {0, 1, 0, 1, 1, 0, 1, 0};
     //int bits[8] = {0, 1, 1, 1, 1, 1, 1, 1};
+    int t[2 * 500 + 1] = {0};
+
     for (int i = 1; i < 2 * n; ++i)
     {
         int a = x[i - 1];
@@ -30,26 +34,27 @@ void update(int x[], int n)
     }
     t[0] = x[0];
     t[2 * n] = x[2 * n];
-    copy(x, t);
+    copy(x, t, 2 * n + 1);
 }
 
 int main()
 {
     int x[2 * 500 + 1] = {0}; // cellular automata
-    x[500] = 1;
-    // x[3] = 1;
-    // x[4] = 1;
+    //x[3] = 1;
+    //x[4] = 1;
     int n; // max of 500 .. the cellular automata is x[0 .. 2 * n + 1]
     std::cin >> n;
+    x[n] = 1;
+    
+    print(x, 2 * n + 1);
+    std::cout << '\n';
 
-    for (int epoch = 0; epoch < 200; ++epoch)
+    for (int epoch = 0; epoch < 2 * n + 1; ++epoch)
     {
-        int t[2 * 500 + 1] = {0};
         update(x, n);
         print(x, 2 * n + 1);
         std::cout << '\n';
     }
-
 
     std::cout << 2 * 2 * 2 << '\n';
     std::cout << (1 << 3) << '\n';
