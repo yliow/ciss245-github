0b075166c0450e881d492826817de5249f554c3c
Yihsiang Liow <yliow@ccis.edu>
2026-03-04 12:56:52 -0800

    no msg

--- 2026-03-04/main.cpp
+++ 2026-03-04/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+struct Date
+{
+    int yyyy_, mm_, dd_;
+};
+
+void init(Date &, int, int, int);
+void print(const Date &);
+void add_y(Date &, int);
+void add_m(Date &, int);
+void add_d(Date &, int);
+
 int main()
 {
     return 0;
