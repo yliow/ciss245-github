291639a575f4755b26d3a2c2f3422e7c15ffadc4
Yihsiang Liow <yliow@ccis.edu>
2026-03-06 13:33:01 -0800

    no msg

---
+++ 2026-03-06/Date/Date.h
@@ -0,0 +1,28 @@
+// Date.h
+
+#ifndef DATE_H
+#define DATE_H
+
+class Date
+{
+  public:
+    Date();
+    Date(int, int, int);
+    Date(const Date &);
+    void init(int, int, int);
+    void print() const;
+    void add_y(int);
+    void add_m(int);
+    void add_d(int);
+    void add_y_m_d(int, int, int);
+    int year() const;
+    int month() const;
+    int day() const;
+    
+  private:
+    int yyyy_, mm_, dd_;
+};
+
+std::ostream & operator<<(std::ostream &, const Date &);
+
+#endif
