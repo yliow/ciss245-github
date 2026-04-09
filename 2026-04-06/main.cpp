40ab270282cd44b463a383f4271c0de2b137301b
Yihsiang Liow <yliow@ccis.edu>
2026-04-09 11:04:03 -0700

    no msg

---
+++ 2026-04-06/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+void hanoi(int n, char from, char helper, char to)
+{
+    if (n == 1)
+    {
+        std::cout << from << " -> " << to << '\n';
+    }
+    else
+    {
+        hanoi(n - 1, from, to, helper);
+        hanoi(1, from, helper, to);
+        hanoi(n - 1, helper, from, to);
+    }
+}
+
+// void print(int x[], int n)
+// {
+//     for (int i = 0; i < n; ++i)
+//     {
+//         std::cout << x[i] << ' ';
+//     }
+//     std::cout << '\n';
+// }
+
+// recursive version of above println
+
+
+
+// void println(int x[], int start, int end)
+// {
+//     for (int i = start; i < end; ++i)
+//     {
+//         std::cout << x[i] << ' ';
+//     }
+//     std::cout << '\n';
+// }
+
+// recursive version of above println
+void println(int x[], int start, int end)
+{
+}
+
+void println(int * start, int * end)
+{
+    for (int * p = start; p < end; ++p)
+    {
+        std::cout << (*p) << ' ';
+    }
+    std::cout << '\n';
+}
+
+// recursive version of above println
+
+int main()
+{
+    int x[] = {1, 2, 3};
+    hanoi(4, 'A', 'B', 'C');
+
+    int x[5] = {7, 5, 2, 4, 6};
+    println(x, 2, 5);
+    return 0;
+}
