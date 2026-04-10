2d338e71982336303387dc5c2668bcb264ae8bb0
Yihsiang Liow <yliow@ccis.edu>
2026-04-10 12:35:51 -0700

    no msg

--- 2026-04-09/main.cpp
+++ 2026-04-09/main.cpp
@@ -54,14 +54,82 @@ void print3(int * start, int * end)
     }
 }
 
+void println3(int * start, int * end)
+{
+    print3(start, end);
+    std::cout << '\n';
+}
+
+void swap(int & a, int & b)
+{
+    int t = a; a = b; b = t;
+}
+
+void bubblesort(int x[], int n)
+{
+    for (int i = n - 2; i >= 0; --i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            if (x[j] > x[j + 1])
+                swap(x[j], x[j + 1]);
+        }
+    }
+}
+
+void bubblesort(int x[], int start, int end)
+{
+    for (int i = end - 2; i >= start; --i)
+    {
+        for (int j = start; j <= i; ++j)
+        {
+            if (x[j] > x[j + 1])
+                swap(x[j], x[j + 1]);
+        }
+    }
+}
+
+void bubblesort_pass(int * start, int * end)
+{
+    int n = end - start;
+    if (n <= 1)
+    {}
+    else
+    {
+        bubblesort_pass(start, end - 1);
+        // for me, i have to handle *(end - 2), *(end - 1)
+        if (*(end - 2) > *(end - 1))
+        {
+            swap(*(end - 2), *(end - 1));
+        }
+    }
+}
+
+void bubblesort(int * start, int * end)
+{
+    int n = end - start;
+    if (n <= 1)
+    {}
+    else
+    {
+        bubblesort_pass(start, end);
+        bubblesort(start, end - 1);
+    }
+    return;
+}
+
 int main()
 {
     int x[] = {5, 3, 1, 2, 4, 6, 8, 7};
     // print0(x, 1, 7); std::cout << '\n';
     // print1(x, 1, 7); std::cout << '\n';
     //print2(x, 1, 7); std::cout << '\n';
-    print3(&x[1], &x[7]);
+    println3(&x[1], &x[7]);
     // linearsrch -- iteration, recrusion
-    // count 
+    // count
+    //bubblesort_pass(&x[0], &x[8]);
+    bubblesort(&x[0], &x[8]);
+    println3(&x[0], &x[8]);
+    
     return 0;
 }
