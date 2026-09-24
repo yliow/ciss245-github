858598717e470676942729ad2d6bb10408ea9e7a
Yihsiang Liow <yliow@ccis.edu>
2026-09-24 12:55:48 -0700

    no msg

--- 2026-09-24/Date/main.cpp
+++ 2026-09-24/Date/main.cpp
@@ -6,6 +6,10 @@ int main()
     Date today = {2026, 9, 23};
     std::cout << today << '\n';
     std::cout << get_year(today) << '\n';
-    
+
+    Date yesterday = get_Date(2026, 9, 22);
+    std::cout << get_year(yesterday) << '\n'; // also get_month, get_day
+    set_year(yesteday, 3000); // also set_month, set_day
+
     return 0;
 }
